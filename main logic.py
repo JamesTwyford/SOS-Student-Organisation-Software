a958893d4bd4bb675fcf62ab8bbcbfea99922f33
JamesTwyford <66450359+JamesTwyford@users.noreply.github.com>
2020-07-15 12:47:00 +1000

    forgot task list.txt

--- main logic.py
+++ main logic.py
@@ -17,7 +17,6 @@ with open("Task List.txt") as fh:
 # EXAMPLE
 Task1Date = TaskList[2]
 Task1Date_obj = datetime.strptime(Task1Date, '%d/%m/%y')
-
 DaysUntilDue = int(str(Task1Date_obj - datetime.today())[0]) + 1
 
 with open("Time Allocations.csv") as file:
@@ -26,13 +25,13 @@ with open("Time Allocations.csv") as file:
         for row in data:
             TimeList.append(row)
 
-Temp = float(0)
+flt_Temp = float(0)
 
 for i in range(1, 15):
     for j in range(1, len(TimeList)):
-        Temp += float(TimeList[j][i])
-    HoursLeft.append(2 - Temp)
-    Temp = float(0)
+        flt_Temp += float(TimeList[j][i])
+    HoursLeft.append(2 - flt_Temp)
+    flt_Temp = float(0)
 
 print(HoursLeft)
 
