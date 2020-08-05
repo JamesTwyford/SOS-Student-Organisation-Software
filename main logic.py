d7f9860b67bab46a8fb8c6694fad043562991f6d
JamesTwyford <66450359+JamesTwyford@users.noreply.github.com>
2020-08-05 11:04:37 +1000

    finished main logic
    just testing then gui intergration then done

--- main logic.py
+++ main logic.py
@@ -9,6 +9,7 @@ import csv
 TaskList = []
 TimeList =[]
 HoursLeft = []
+NewTimeList = []
 
 with open("Task List.txt") as fh:
     for line in fh:
@@ -16,8 +17,8 @@ with open("Task List.txt") as fh:
 
 # EXAMPLE
 Task1Date = TaskList[2]
-Task1Date_obj = datetime.strptime(Task1Date, '%d/%m/%y')
-DaysUntilDue = int(str(Task1Date_obj - datetime.today())[0]) + 1
+obj_Task1Date = datetime.strptime(Task1Date, '%d/%m/%y')
+int_DaysUntilDue = int(str(obj_Task1Date - datetime.today())[0])
 
 with open("Time Allocations.csv") as file:
     for i in range(1, 14):
@@ -25,15 +26,74 @@ with open("Time Allocations.csv") as file:
         for row in data:
             TimeList.append(row)
 
-flt_Temp = float(0)
+def CSVWriteFunc(y, x, TimeNeeded):
+    NewTimeList = TimeList
+    x += 1
+    NewTimeList[y][x] = float(NewTimeList[y][x]) + TimeNeeded
+    with open("Time Allocations.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(NewTimeList)
 
-for i in range(1, 15):
-    for j in range(1, len(TimeList)):
-        flt_Temp += float(TimeList[j][i])
-    HoursLeft.append(2 - flt_Temp)
-    flt_Temp = float(0)
+# here we GOOOOO
+flt_Temp = 0
 
-print(HoursLeft)
+for i in range(1, len(TimeList)): #repeat the amount of times = to the number of tasks - i is the task i am on
+    HoursLeft = []
+    int_TimeNeeded = int(TaskList[i*3]) #Time needed on the first task
+    for m in range(1, 15): # m is determining the hours left
+        flt_Temp = float(TimeList[i][m])
+        HoursLeft.append(2 - flt_Temp)
+        flt_Temp = 0
+    for j in range(0, 14):
+        if int_TimeNeeded == 0:
+            pass
+        else:
+            if HoursLeft[j] == 0:
+                pass
+            elif int_TimeNeeded > 10:
+                int_TimeNeeded -= 2
+                CSVWriteFunc(i, j, 2)
+            elif j < 4 and HoursLeft[j] >= 0.5:
+                if int_TimeNeeded == 0.5:
+                    int_TimeNeeded -= 0.5
+                    CSVWriteFunc(i, j, 0.5)
+                else:
+                    int_TimeNeeded -= 1
+                    CSVWriteFunc(i, j, 1)
+            elif 14 - j < 3 and HoursLeft[j] == 2:
+                if int_TimeNeeded >= 2:
+                    int_TimeNeeded -= 2
+                    CSVWriteFunc(i, j, 2)s
+                else:
+                    int_TimeNeeded -= int_TimeNeeded
+                    CSVWriteFunc(i, j, int_TimeNeeded)
+            elif HoursLeft[j] == 2:
+                if int_TimeNeeded >= 2:
+                    int_TimeNeeded -= 1
+                    CSVWriteFunc(i, j, 1)
+                else:
+                    if int_TimeNeeded >= 1:
+                        int_TimeNeeded -= 1
+                        CSVWriteFunc(i, j, 1)
+                    else:
+                        int_TimeNeeded -= int_TimeNeeded
+                        CSVWriteFunc(i, j, int_TimeNeeded)
+            elif HoursLeft[j] >= 1:
+                if int_TimeNeeded >= 1:
+                    int_TimeNeeded -= 0.5
+                    CSVWriteFunc(i, j, 0.5)
+                else:
+                    int_TimeNeeded -= int_TimeNeeded
+                    CSVWriteFunc(i, j, int_TimeNeeded)
+            elif HoursLeft[j] == 0.5:
+                int_TimeNeeded -= 0.5
+                CSVWriteFunc(i, j, 0.5)
+
+if int_TimeNeeded > 0:
+    print("my friend, you have fucked up")
+
+
+
 
 
 
